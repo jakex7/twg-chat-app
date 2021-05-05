096d0e42cba91aee03bdcce8f2d1332b2cb339ed
Jakub Grzywacz <jake@Jakubs-iMac.local>
2021-05-05 15:13:30 +0200

    Basic configuration prettier and eslint with airbnb configuration

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    'react-native/react-native': true,
+  },
+  plugins: ['react', 'react-native', 'react-hooks'],
+  parser: 'babel-eslint',
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'react/style-prop-object': 'off',
+    //   'comma-dangle': 'off',
+    //   'padded-blocks': 'off',
+    //   'arrow-body-style': 'off',
+    //   'react-hooks/exhaustive-deps': 'warn',
+    //   'react-native/no-unused-styles': 2,
+    //   'react-native/split-platform-components': 2,
+    //   'react-native/no-inline-styles': 2,
+    //   'react-native/no-color-literals': 2,
+    //   'react-native/no-raw-text': 2,
+    //   'react-native/no-single-element-style-arrays': 2,
+  },
+};
