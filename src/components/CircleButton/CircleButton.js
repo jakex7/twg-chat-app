fdb9b8c4646b6ff7836749ff80f88a0d0c0c223a
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-05-06 10:53:17 +0200

    Add Circle button

---
+++ src/components/CircleButton/CircleButton.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import Profile from '../../assets/images/profile.svg';
+import Search from '../../assets/images/search.svg';
+import Rooms from '../../assets/images/rooms.svg';
+import Phone from '../../assets/images/phone.svg';
+import VideoCall from '../../assets/images/videocall.svg';
+
+const CircleButton = ({ iconName = '', handlePress = () => {}, ...rest }) => {
+  const props = {
+    height: '44',
+    width: '44',
+    style: {
+      borderRadius: 44 / 2,
+    },
+  };
+  return (
+    <View {...rest}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handlePress}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {!iconName && <View style={{ width: 24, height: 24 }} />}
+            {iconName === 'profile' && <Profile {...props} />}
+            {iconName === 'search' && <Search {...props} />}
+            {iconName === 'rooms' && <Rooms {...props} />}
+            {iconName === 'phone' && <Phone {...props} />}
+            {iconName === 'videocall' && <VideoCall {...props} />}
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    width: 44,
+    height: 44,
+    borderRadius: 44 / 2,
+  },
+});
+
+export default CircleButton;
